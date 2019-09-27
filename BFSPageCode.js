600a26a4efc0a0aa5df9c5649c8909e3fff00dc6
shmad <50178810+shmad@users.noreply.github.com>
2019-09-26 21:41:31 -0400

    almost there

--- BFSPageCode.js
+++ BFSPageCode.js
@@ -40,10 +40,10 @@ const gridInit = () =>{
             cell.style.paddingTop = "2px";
             cell.onmousedown=function(){mouseDown()};
             cell.ontouchstart=function(){mouseDown()};
-            cell.ontouchmove = function(){changeCell(this.id,this.parentElement.id)};
+            
             cell.ontouchend = function(){mouseUp()};
             cell.onmouseup=function(){mouseUp()};
-            cell.onmouseover=function(){changeCell(this.id,this.parentElement.id)};
+            cell.onpointerover=function(){changeCell(this.id,this.parentElement.id)};
             cell.style.touchAction = "none";
             p.id = "p"+i;
             p.style.touchAction = "none";
@@ -115,10 +115,10 @@ function newGrid(rows,cols,grid){
             cell.style.paddingTop = "2px";
             cell.onmousedown=function(){mouseDown()};
             cell.ontouchstart=function(){mouseDown()};
-            cell.ontouchmove = function(){changeCell(this.id,this.parentElement.id)};
+            
             cell.ontouchend = function(){mouseUp()};
             cell.onmouseup=function(){mouseUp()};
-            cell.onmouseover=function(){changeCell(this.id,this.parentElement.id)};
+            cell.onpointerover=function(){changeCell(this.id,this.parentElement.id)};
             cell.style.touchAction = "none";
             p.id = "p"+i;
             p.style.touchAction = "none";
